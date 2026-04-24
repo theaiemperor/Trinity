834f23d05f444b13afdd3f516b8f50b483189da8
Arman <theaiemperor@gmail.com>
2026-04-24 16:56:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## About
+
+A Demo website for trinity
---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  "output": "export",
+  devIndicators: {
+    position: 'bottom-right'
+  }
+};
+
+export default nextConfig;
